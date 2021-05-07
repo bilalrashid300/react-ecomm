6a0b3b005453d51a8b7019d7ddc1473ba92e5e0f
bilal rashid <bilalrashid098@gmail.com>
2021-05-07 15:35:10 +0500

    conversion of state into react hook : useState

--- src/pages/Recovery/index.js
+++ src/pages/Recovery/index.js
@@ -1,127 +1,137 @@
-import {react, Component} from 'react';
+import {react, useState} from 'react';
 import {signInWithGoogle, auth} from '../../firebase/utils'
 import Button from '../../components/forms/button/index'
 import Input from '../../components/forms/formInput/index'
 import './style.css'
 
-const initialState = {
-    email: '',
-    errors: [],
-    success: []
-}
 
-class Recovery extends Component {
+const Recovery = props =>  {
+    
+    const [email,setEmail] = useState('')
+    const [success,setSuccess] = useState([])
+    const [errors,setError] = useState([])
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...initialState
-        }
-        // this.forceUpdateHandler = this.forceUpdateHandler.bind(this)
+    const reset = () => {
+        setEmail('');
+        setSuccess([]);
+        setError([]);
     }
 
-    
-    // forceUpdateHandler = () =>{
-    //     this.forceUpdate();
-    // };
-    
-    handleSubmit = async e => {
+    const handleSubmit = async e => {
         e.preventDefault();
         
-        this.setState({
-            errors: [],
-        })
+        setError([])
 
-        const {email,errors, success} = this.state;
         const err = [];
         const successCollection = [];
 
         if(email === ""){
-            const err = "Email feild is empty"
-            errors.push(err);
-            this.setState({
-                errors: errors
-            })
+            const err1 = "Email feild is empty"
+            err.push(err1);
+            setError(err);
         }
         else{
-            try {
-                
-                const config = {
-                    url:'http://localhost:3000/login'
-                }
-
-
-                await auth.sendPasswordResetEmail(email,config)
-                    .then(() =>{
-                        const successCollection = "Password Recovery link has been sent to your email"
-                        success.push(successCollection);
-                        this.setState({
-                            success: success
-                        })
-                    })
-                    .catch( () =>{
-                        const err = "Email doesnt exist"
-                        errors.push(err);
-                        this.setState({
-                            errors: errors
-                        })
-                    })
-                this.setState({
-                    ...initialState
+            const config = {
+                url:'http://localhost:3000/login'
+            }
+            await auth.sendPasswordResetEmail(email,config)
+                .then((res) => {
+                    const successCollection1 = "Password Recovery link has been sent to your email"
+                    successCollection.push(successCollection1);
+                    setSuccess(successCollection);
+                    console.log(res)
+                    if(res !== undefined && res.code === "auth/user-not-found"){
+                        console.log(res.message);
+                    }
                 })
-            }
-            catch (err) {
-                console.error(err)
-            }
+                .catch((err) => {
+                    const successCollection1 = "Password Recovery link has been sent to your email"
+                    successCollection.push(successCollection1);
+                    setSuccess(successCollection);
+                    console.log(err)
+                })
+
+            reset();
         }  
     }
 
-    handleChange = (e) => {
-        const {name, value} = e.target;
-        this.setState({
-            [name] : value
-        })
-    }
 
-    render(){
+    // try {
+    //     const res =  await auth.sendPasswordResetEmail(email,config);
+    //     if(res !== undefined && res.code === "auth/user-not-found"){
+    //         console.log(res.message);
+    //     }
+    //     else{
+    //         console.log(res);
+    //     }
+    //     //   const successCollection1 = "Password Recovery link has been sent to your email"
+    //     //       successCollection.push(successCollection1);
+    //     //       setSuccess(successCollection);
+    //     //       console.log(successCollection)
+    // } catch (error) {
+    //     // console.log(error);
+    //     // const err2 = "Email doesnt exist"
+    //     // err.push(err2);
+    //     // setError(err);
+    //     if (error.response) {
+    //         /*
+    //          * The request was made and the server responded with a
+    //          * status code that falls out of the range of 2xx
+    //          */
+    //         console.log(error.response.data);
+    //         console.log(error.response.status);
+    //         console.log(error.response.headers);
+    //     } else if (error.request) {
+    //         /*
+    //          * The request was made but no response was received, `error.request`
+    //          * is an instance of XMLHttpRequest in the browser and an instance
+    //          * of http.ClientRequest in Node.js
+    //          */
+    //         console.log(error.request);
+    //     } else {
+    //         // Something happened in setting up the request and triggered an Error
+    //         console.log('Error: ', error.message);
+    //     }
+    // }
 
-        const {email, errors, success} = this.state;
-
-        return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 ">
-                        <h1 class="mb-4 text-center">Recover Password</h1>
-                        <form onSubmit={this.handleSubmit}>
-                            <Input type="email" label="Email" name="email" value={email} placeholder="Email" onChange={this.handleChange}  required/>
-                            {success.length > 0 && (
-                                
-                                <ul className="successToken">
-                                    {success.map( (success,index2) => {
-                                        return(
-                                            <li key={index2}>{success}</li>
-                                        )
-                                    })}
-                                </ul>
-                            )}
-                            {errors.length > 0 && (
-                                <ul className="errorToken">
-                                    {errors.map( (error,index) => {
-                                        return (
-                                            <li key={index}>{error}</li>
-                                        )
-                                    })}
-                                </ul>
-                            )}
-                            <div className="text-center mt-5">
-                                <Button>Send</Button>
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 ">
+                    <h1 className="mb-4 text-center">Recover Password</h1>
+                    <form onSubmit={handleSubmit}>
+                        <Input type="email" label="Email" name="email" value={email} placeholder="Email" onChange={e => setEmail(e.target.value)}  required/>
+                        { success.length > 0 || (errors.length > 0 && (
+                             <div className="mt-3 pt-3 border-top"> 
+                                {success.length > 0 && (
+                                    
+                                    <ul className="successToken">
+                                        {success.map( (success,index2) => {
+                                            return(
+                                                <li key={index2}>{success}</li>
+                                            )
+                                        })}
+                                    </ul>
+                                )}
+                                {errors.length > 0 && (
+                                    <ul className="errorToken">
+                                        {errors.map( (error,index) => {
+                                            return (
+                                                <li key={index}>{error}</li>
+                                            )
+                                        })}
+                                    </ul>
+                                )}
                             </div>
-                        </form>
-                    </div>
+                        ))}
+                        <div className="text-center mt-5">
+                            <Button>Send</Button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Recovery
--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,108 +1,98 @@
-import {react, Component} from 'react';
+import {react, useState} from 'react';
 import {auth, handleUserProfile} from '../../firebase/utils'
 import Input from '../../components/forms/formInput/index'
 import Button from '../../components/forms/button/index'
 import './style.css'
 
-const initialState = {
-    displayName: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    errors: []
-}
 
-class Signin extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...initialState
-        }
+const Signin = props => {
+
+    const [displayName,setDisplayName] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [confirmPassword,setConfirmPassword] = useState('')
+    const [errors,setErrors] = useState('')
+
+    const reset = () => {   
+        setDisplayName('');
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+        setErrors('');
     }
 
-    handleChange = (e) => {
-        const {name, value} = e.target;
-        this.setState({
-            [name]: value
-        })
-    }
 
-    handleFormSubmit = async e => {
+    const handleFormSubmit = async e => {
         e.preventDefault();
-        const {displayName, email, password, confirmPassword, errors} = this.state;
         const err = [];
         if(password !== confirmPassword || displayName === "" || email === ""){
             if( password !== confirmPassword){
-                const err = ['Password does not match']; 
-                errors.push(err);
-                this.setState({
-                    errors: errors
-                })
+                const err1 = ['Password does not match']; 
+                err.push(err1);
+                setErrors(err);
             }
             if( displayName === "" ){
-                const err = ['Name is required']; 
-                errors.push(err);
-                this.setState({
-                    errors: errors
-                })
+                const err2 = ['Name is required']; 
+                err.push(err2);
+                setErrors(err);
             }
             if( email === "" ){
-                const err = ['Email is required']; 
-                errors.push(err);
-                this.setState({
-                    errors: errors
-                })
+                const err3 = ['Email is required']; 
+                err.push(err3);
+                setErrors(err);
+            }
+            if( password === "" ){
+                const err4 = ['Password is required']; 
+                err.push(err4);
+                setErrors(err);
+            }
+            if( confirmPassword === "" ){
+                const err5 = ['Confirm password is required']; 
+                err.push(err5);
+                setErrors(err);
             }
             return
         }
         else{
-            const {displayName} = this.state;
+            document.getElementById('errorList').remove();
             try{
                 const { user } = await auth.createUserWithEmailAndPassword(email,password)
-                console.log('Username',{displayName})
                 await handleUserProfile( user, {displayName});
             }
             catch(e){
                 console.log(e)
             }
-            this.setState({
-                ...initialState
-            })
+            reset()
         }
     }  
 
-    render() {
-
-        const {displayName, email, password, confirmPassword, errors} = this.state
-
-        return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-6">
-                        <h1 className="text-center">Signin</h1>
-                        <form onSubmit={this.handleFormSubmit}>
-                            <Input type="text" label="Name" name="displayName" value={displayName} placeholder="Full Name" onChange={this.handleChange}  required />
-                            <Input type="email" label="Email" name="email" value={email} placeholder="Email" onChange={this.handleChange}  required />
-                            <Input type="password" label="Password" name="password" value={password} placeholder="Password" onChange={this.handleChange} required />  
-                            <Input type="password" label="Confirm Password" name="confirmPassword" value={confirmPassword} placeholder="Confirm Password" onChange={this.handleChange} required />
-                            <div className="text-center mt-5">
-                                <Button>Submit</Button>
-                            </div>
-                            {errors.length > 0 &&(
-                                <ul>
-                                    {errors.map((err,index) => {
-                                        return(
-                                            <li key={index}>{err}</li>
-                                        )
-                                    })}
-                                </ul>
-                            )}
-                        </form>
-                    </div>
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6">
+                    <h1 className="text-center">Signin</h1>
+                    <form onSubmit={handleFormSubmit}>
+                        <Input type="text" label="Name" name="displayName" value={displayName} placeholder="Full Name" onChange={e => setDisplayName(e.target.value)}   />
+                        <Input type="email" label="Email" name="email" value={email} placeholder="Email" onChange={e => setEmail(e.target.value)}   />
+                        <Input type="password" label="Password" name="password" value={password} placeholder="Password" onChange={e => setPassword(e.target.value)}  />  
+                        <Input type="password" label="Confirm Password" name="confirmPassword" value={confirmPassword} placeholder="Confirm Password" onChange={e => setConfirmPassword(e.target.value)}  />
+                        <div className="text-center mt-5">
+                            <Button>Submit</Button>
+                        </div>
+                        {errors.length > 0 &&(
+                            <ul id="errorList" className=" mt-3 pt-3 border-top errorToken">
+                                {errors.map((err,index) => {
+                                    return(
+                                        <li key={index}>{err}</li>
+                                    )
+                                })}
+                            </ul>
+                        )}
+                    </form>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Signin
